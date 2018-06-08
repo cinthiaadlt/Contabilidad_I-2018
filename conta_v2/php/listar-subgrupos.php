2579850d9ce012358be86ec1261a554abbe40b62
wenguz <wenguz1518@gmail.com>
2018-06-08 13:58:00 -0400

    prueba 2
    prueba listar subgrupos

--- conta_v2/php/listar-subgrupos.php
+++ conta_v2/php/listar-subgrupos.php
@@ -1,30 +1,10 @@
-<?php
-/*~ Archivo listar-subgrupos.php
-.---------------------------------------------------------------------------.
-|    Software: CAS - Computerized Accountancy System                        |
-|     Versión: 1.0                                                          |
-|   Lenguajes: PHP, HTML, CSS3 y Javascript                                 |
-| ------------------------------------------------------------------------- |
-|   Autores: Ricardo Vigil (alexcontreras@outlook.com)                      |
-|          : Vanessa Campos                                                 |
-|          : Ingrid Aguilar                                                 |
-|          : Jhosseline Rodriguez                                           |
-| Copyright (C) 2013, FIA-UES. Todos los derechos reservados.               |
-| ------------------------------------------------------------------------- |
-|                                                                           |
-| Este archivo es parte del sistema de contabilidad C.A.S para la cátedra   |
-| de Sistemas Contables de la Facultad de Ingeniería y Arquitectura de la   |
-| Universidad de El Salvador.                                               |
-|                                                                           |
-'---------------------------------------------------------------------------'
-*/
-?>
 <?php 
 	include("sesion.php");
 	if(!$_COOKIE["sesion"]){
 		header("Location: salir.php");
 	}
 ?>
+
 <!DOCTYPE html>
 <html lang="es">
 <head>
