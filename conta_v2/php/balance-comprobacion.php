54dec3a3c2e7de434ce394f14395ec6292898e05
wenguz <wenguz1518@gmail.com>
2018-06-14 02:33:56 -0400

    PDF balance comprobacion
    generar pdf del balance de comprobacion

--- conta_v2/php/balance-comprobacion.php
+++ conta_v2/php/balance-comprobacion.php
@@ -1,3 +1,167 @@
+<?php
+if(!isset($conexion)){
+	include("conexion.php");
+	/*consulata para obtenr totales segun subgrupo*/
+    $consulta = "SELECT DISTINCT(c.codigo_cuenta),c.subgrupo,SUM((c.saldo_debe)) sumdebe,SUM((c.saldo_haber)) sumhaber FROM cuentas c,subgrupos s WHERE c.subgrupo=s.codigo_subgrupo GROUP by c.subgrupo";
+                               		$consulta = $conexion->query($consulta);
+if(isset($_POST['create_pdf'])){
+  include('../reportes_pdf/tcpdf/tcpdf.php');
+    
+    $pdf = new TCPDF('P', 'mm', 'A4', true, 'UTF-8', false);
+
+    $pdf->SetCreator(PDF_CREATOR);
+    $pdf->SetAuthor('Contabilidad vicaria');
+    $pdf->SetTitle($_POST['reporte_name']);
+
+    $pdf->setPrintHeader(false);
+    $pdf->setPrintFooter(false);
+    $pdf->SetMargins(20, 20, 20, false);
+    $pdf->SetAutoPageBreak(true, 20);
+    $pdf->SetFont('Helvetica', '', 10);
+    $pdf->addPage();
+     $content .= '
+    <div class="container" id="contenido">
+        <div class="row row-offcanvas row-offcanvas-right">
+            <div class="col-xs-12 col-sm-9">
+                  <h3 style="text-align:center;">BALANCE DE COMPROBACION</h3>
+                <div class="col-lg-12">
+                        <table class="table table-condensed table-bordered table-striped" border="1" cellpadding="5">
+                            <thead >
+                                <tr>
+                                    <th align="center"><strong>CUENTA</strong></th>
+                                    <th align="center"><strong>DEBE</strong></th>
+                                    <th align="center"><strong>HABER</strong></th>
+                                    <th align="center"><strong>SALDO DEBE</strong></th>
+                                    <th align="center"><strong>SALDO HABER</strong></th>
+                                </tr>
+                            </thead>
+    ';
+    						/*Suma segun subgurupos de cuentas*/
+                                while ($subg = $consulta->fetch_assoc()) {
+									$sql = "SELECT * FROM cuentas where subgrupo='".$subg["subgrupo"]."' ";
+									$ejecutar = $conexion->query($sql);
+									$deudor=0;
+									$acreedor=0;
+									while($regs = $ejecutar->fetch_assoc()){
+										$content.='
+											<tr>
+											<td>'.utf8_encode($regs["codigo_cuenta"]).''.utf8_encode($regs["nombre_cuenta"]).'</td>
+										';
+										if($regs["saldo_debe"]==0){
+											$deudor =$deudor+0; 
+                                            $acreedor =$acreedor+($regs["saldo_haber"]-$regs["saldo_haber"]);
+                                            $content.='
+                                            <td class="text-right">'.number_format($regs["saldo_debe"],2).'</td>
+                                            <td class="text-right">'.number_format($regs["saldo_haber"],2).'</td>
+                                            <td align="right">$'.number_format($deudor, 2).'</td>
+                                            <td align="right">$'.number_format($acreedor, 2).'</td>
+                                            ';
+                                        }elseif ($regs["saldo_haber"]==0){
+                                            $deudor = $deudor+$regs["saldo_debe"]-$regs["saldo_haber"];
+                                            $acreedor = $acreedor+0;
+                                            $content.='
+                                            <td class="text-right">'.number_format($regs["saldo_debe"],2).'</td>
+                                            <td class="text-right">'.number_format($regs["saldo_haber"],2).'</td>
+                                            <td align="right">$ '.number_format($deudor, 2).'</td>
+                                            <td align="right">$ '.number_format($acreedor, 2).'</td>
+                                            ';
+										}elseif ($regs["saldo_debe"]<$regs["saldo_haber"]) {
+                                            if ($acreedor>$regs["saldo_debe"]) {
+                                            	$acreedor=$acreedor-$regs["saldo_haber"];
+                                            }
+                                            else{
+                                             	$acreedor=$acreedor-$regs["saldo_debe"];
+                                            }
+                                            $deudor =$deudor+ 0;
+                                            $acreedor =$acreedor+ ($regs["saldo-haber"]-$regs["saldo_debe"]);  
+                                            $content.=' 
+                                            <td class="text-right">'.number_format($regs["saldo_debe"],2).'</td>
+                                            <td class="text-right">'.number_format($regs["saldo_haber"],2).'</td>
+                                            <td align="right">$'.number_format($deudor, 2).'</td>
+                                            <td align="right">$'.number_format($acreedor, 2).'</td>
+                                            ';
+                                        }
+                                        elseif ($regs["saldo_debe"]>$regs["saldo_haber"]) {
+                                            if ($deudor>$regs["saldo_haber"]) {
+                                             	$deudor=$deudor-$regs["saldo_debe"];
+                                            }
+                                            else{
+                                            	$deudor=$deudor-$regs["saldo_haber"];
+                                            }
+                                            $deudor = $deudor+($regs["saldo_debe"]-$regs["saldo_haberbe"]);
+                                            $acreedor = $acreedor+ 0;
+                                            $content.=' 
+                                            <td class="text-right">'.number_format($regs["saldo_debe"],2).'</td>
+                                            <td class="text-right">'.number_format($regs["saldo_haber"],2).'</td>
+                                            <td align="right">$'.number_format($deudor, 2).'</td>
+                                            <td align="right">$'.number_format($acreedor, 2).'</td>
+                                            ';
+                                        }
+                                        $content.='</tr>';
+									}
+									$content.='<tr bgcolor="#A4A4A4">
+                                        <td align="right" colspan="3"><strong>Sumas Totales:</strong></td>
+                                        <td align="right">'.number_format($deudor,2).'</td>
+                                        <td align="right">'.number_format($acreedor,2).'</td>
+                                    </tr>';
+								}
+									/*Total de todas las cuentas*/ 
+									$sql = "SELECT SUM(saldo_debe) sumadebe, SUM(saldo_haber) sumahaber FROM cuentas";
+									$ejecutar = $conexion->query($sql);
+									$content.='<tr bgcolor="#f5f5f5">';
+									while($reg = $ejecutar->fetch_assoc()){
+											if ($reg["sumadebe"]<$reg["sumahaber"]) {
+                                                    $deudor = 0;
+                                                    $acreedor = $reg["sumahaber"]-$reg["sumadebe"];
+                                                }
+                                                elseif ($reg["sumdebe"]>$reg["sumhaber"]) {
+                                                    $deudor = $reg["sumdebe"]-$reg["sumhaber"];
+                                                    $acreedor = 0;
+                                                }
+
+										if($reg["sumadebe"]!=$reg["sumahaber"]){
+											$content.='<td class="danger"><strong>Totales:</strong> </td>
+											<td class="text-right danger"><strong>'.number_format($reg["sumadebe"],2).'</strong></td>
+											<td class="text-right danger"><strong>'.number_format($reg["sumahaber"],2).'</strong></td>
+											';
+											if ($reg["sumadebe"]>=$reg["sumahaber"]) {
+												$content.='<td class="text-right danger">$ '.number_format($reg["sumadebe"]-$reg["sumahaber"], 2).'</td>
+												<td class="text-right danger">$ '.number_format(0, 2).'</td>
+												';
+											}
+											else{
+												$content.='<td class="text-right danger">$'.number_format(0, 2).'</td>
+												<td class="text-right danger">$'.number_format($reg["sumahaber"]-$reg["sumadebe"], 2).'</td>
+												';
+											}
+										} else {
+                                            $content.='
+                                            <td><strong>Totales:</strong> </td>
+                                            <td class="text-right"><strong>'.number_format($reg['sumadebe'],2).'</strong></td>
+                                            <td class="text-right"><strong>'.number_format($reg['sumahaber'],2).'</strong></td>
+											<td class="text-right danger">$ '.number_format('0', 2).'</td>
+                                            <td class="text-right danger">$ '.number_format('0', 2).'</td>
+                                            ';
+										}	
+									}
+									$content.='</tr>';
+                        $content.='
+                        </table>
+                </div>
+            </div>
+        </div>
+    </div>
+                        ';
+    $pdf->writeHTMLCell(0, 0, '', '', $content, 0, 1, 0, true, '', true);
+    ob_end_clean();
+    $pdf->output('Reporte.pdf', 'I');
+}
+}
+?>
+
+
+
+<!--CONTENIDO DE LA PAGINA--->
 <?php 
 	include("sesion.php");
 	if(!$_COOKIE["sesion"]){
@@ -27,7 +191,9 @@
 		<div class="row row-offcanvas row-offcanvas-right">
 			<div class="col-xs-12 col-sm-9">
 				<div class="page-header">
-        			<h3>Balance de Comprobación</h3>
+        			<?php $h1 = "Balance de Comprobacion";
+                        echo '<h3>'.$h1.'</h3>'
+                    ?>
         		</div>
         		<div class="row">
         			<div class="container">
@@ -37,11 +203,6 @@
 									<th colspan="6">
 									<!--Nombre de la entidad -->
 										<h2 class="text-center">Balance de Comprobación</h2>
-										<!--
-										<p align="center">
-											<strong>Balance de Comprobación</strong>
-										</p>
-										-->
 										<p align="center">
 											<script>
 												var month=new Array();
@@ -74,11 +235,7 @@
 								</tr>
 								<?php 
 								error_reporting(E_ALL ^ E_NOTICE);
-								if(!isset($conexion)){
-									include("conexion.php");
-									/*consulata para obtenr totales segun subgrupo*/
-                                	$consulta = "SELECT DISTINCT(c.codigo_cuenta),c.subgrupo,SUM((c.saldo_debe)) sumdebe,SUM((c.saldo_haber)) sumhaber FROM cuentas c,subgrupos s WHERE c.subgrupo=s.codigo_subgrupo GROUP by c.subgrupo";
-                               		$consulta = $conexion->query($consulta);
+								
                                		
                                     /*Suma segun subgurupos de cuentas*/
                                 while ($subg = $consulta->fetch_assoc()) {
@@ -136,11 +293,10 @@
 											echo "</tr>";
 									}
 									echo "<tr>";
-                                                echo "<td class='text-right' colspan='3'><strong>Sumas Totales:</strong></td>";
-                                              
-                                                echo "<td align='right'>".number_format($deudor,2)."</td>";
-                                                echo "<td align='right'>".number_format($acreedor,2)."</td>";
-                                                echo "</tr>";
+                                    echo "<td class='text-right' colspan='3'><strong>Sumas Totales:</strong></td>";
+                                    echo "<td align='right'>".number_format($deudor,2)."</td>";
+                                    echo "<td align='right'>".number_format($acreedor,2)."</td>";
+                                    echo "</tr>";
 								}
 									/*Total de todas las cuentas*/ 
 									$sql = "SELECT SUM(saldo_debe) sumadebe, SUM(saldo_haber) sumahaber FROM cuentas";
@@ -177,17 +333,21 @@
 											echo "<td class='text-right danger'>$ ".number_format('0', 2)."</td>";
                                             echo "<td class='text-right danger'>$ ".number_format('0', 2)."</td>";
 										}
-										
-											
 									}
 									echo "</tr>";
-								}
+								
 								?>
 							</tbody>
         				</table>
         				
         			</div>
         		</div>
+        	<div class="col-md-12">
+                <form method="post">
+                    <input type="hidden" name="reporte_name" value="<?php echo $h1; ?>">
+                    <input type="submit" name="create_pdf" class="btn btn-danger pull-right" value="Generar PDF">
+                </form>
+            </div>
         	</div><!--/span-->
 
 			<!-- Barra lateral o sidebar -->
